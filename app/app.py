2e48e8debd087c65efa5fbe7ba8b3fc86ad841ed
involodya <novikov.vp@phystech.edu>
2025-05-18 18:06:07 +0300

    hw2

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://postgres:securepassword@db:5432/taskapp')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['STATIC_FOLDER'] = os.environ.get('STATIC_FOLDER', 'static')
 
--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 set -e
 
 python -c "from app import wait_for_db, initialize_database; wait_for_db() and initialize_database()"
